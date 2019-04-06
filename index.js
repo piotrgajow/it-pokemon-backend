b275e67901e022665c3cf6bc68eb270c07e259d5
Piotr Gajowniczek <piotrgajow@gmail.com>
2019-04-07 01:08:06 +0200

    Change port to 8090

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const PORT = 8080;
+const PORT = 8090;
 const QUESTIONS_PER_QUIZ = 10;
 
 const questions = require('./questions');
